b07d3dc53b704e8f28529727e83c5bb260301b55
Daniel Armengod <darmengod@UBM.local>
2019-09-13 14:37:21 +0200

    Subida de inicio de sesion, solamente recoge los datos y falta que los mande al controlador. (No lo hace correctamente)

--- src/main.js
+++ src/main.js
@@ -5,4 +5,26 @@ Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  el: '#app',
+  data:{
+    errors: [],
+    username:'',
+    password:''
+  },
+  methods:{
+    checkJoinned : function(e) {
+      
+      if( !this.username ){
+        this.errors.push("Debe de informar del Nombre de Usuario.");
+      }
+      if( !this.password ){
+        this.errors.push("Debe de informar de la contraseña.");
+      }
+      
+      if( this.username && this.password ){
+        return true;
+      }
+      e.preventDefault();
+    }
+  }
 }).$mount('#app')
